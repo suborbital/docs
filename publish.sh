cee9cedb463480b0d3e4ad1083076f3495d4ed39
Flaki <git@flaki.hu>
2022-03-11 16:30:23 +0200

    Use GITHUB_HEAD_REF for pull request source

--- publish.sh
+++ publish.sh
@@ -4,10 +4,8 @@ set -e
 INPUT_BRANCH=${INPUT_BRANCH:-gh-pages}
 INPUT_DIRECTORY=${INPUT_DIRECTORY:-'website/build'}
 OUTPUT_DIRECTORY=${OUTPUT_DIRECTORY:-'gh-pages'}
-OUTPUT_SUBDIR=${OUTPUT_DIRECTORY}/${GITHUB_REF_NAME}
+OUTPUT_SUBDIR=${OUTPUT_DIRECTORY}/${GITHUB_HEAD_REF}
 echo "Publishing INPUT_BRANCH=${INPUT_BRANCH} >> OUTPUT_SUBDIR=${OUTPUT_SUBDIR}";
-echo "This is a test, stopping deployment."
-exit 0;
 
 echo "Push to branch $INPUT_BRANCH";
 [ -z "${GITHUB_TOKEN}" ] && {
@@ -20,9 +18,11 @@ remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSIT
 rm -rf "${OUTPUT_DIRECTORY}"
 git clone --branch "${INPUT_BRANCH}" "${remote_repo}" "${OUTPUT_DIRECTORY}"
 mv "${OUTPUT_DIRECTORY}/.git" output.git
-rm -rf "${OUTPUT_DIRECTORY}"
-cp -r "${INPUT_DIRECTORY}" "${OUTPUT_DIRECTORY}"
+mkdir -p "${OUTPUT_SUBDIR}"
+rm -rf "${OUTPUT_SUBDIR}"
+cp -r "${INPUT_DIRECTORY}" "${OUTPUT_SUBDIR}"
 
+rm -rf "${OUTPUT_SUBDIR}/.git"
 mv output.git "${OUTPUT_DIRECTORY}/.git"
 cd "${OUTPUT_DIRECTORY}"
 touch .nojekyll
