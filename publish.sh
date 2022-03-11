f5427ff8244df9090fe6e841353dfa9a65dacc0a
Flaki <git@flaki.hu>
2022-03-11 17:22:25 +0200

    Publish non-PRs in 'preview'

--- publish.sh
+++ publish.sh
@@ -4,7 +4,8 @@ set -e
 INPUT_BRANCH=${INPUT_BRANCH:-gh-pages}
 INPUT_DIRECTORY=${INPUT_DIRECTORY:-'website/build'}
 OUTPUT_DIRECTORY=${OUTPUT_DIRECTORY:-'gh-pages'}
-OUTPUT_SUBDIR=${OUTPUT_DIRECTORY}/${GITHUB_HEAD_REF}
+DEPLOY_TARGET=${GITHUB_HEAD_REF:-'preview'}
+OUTPUT_SUBDIR=${OUTPUT_DIRECTORY}/${DEPLOY_TARGET}
 echo "Publishing INPUT_BRANCH=${INPUT_BRANCH} >> OUTPUT_SUBDIR=${OUTPUT_SUBDIR}";
 
 echo "Push to branch $INPUT_BRANCH";
@@ -18,11 +19,11 @@ remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSIT
 rm -rf "${OUTPUT_DIRECTORY}"
 git clone --branch "${INPUT_BRANCH}" "${remote_repo}" "${OUTPUT_DIRECTORY}"
 mv "${OUTPUT_DIRECTORY}/.git" output.git
-mkdir -p "${OUTPUT_SUBDIR}"
 rm -rf "${OUTPUT_SUBDIR}"
+mkdir -p "${OUTPUT_SUBDIR}"
 cp -r "${INPUT_DIRECTORY}" "${OUTPUT_SUBDIR}"
 
-rm -rf "${OUTPUT_SUBDIR}/.git"
+rm -rf "${OUTPUT_DIRECTORY}/.git"
 mv output.git "${OUTPUT_DIRECTORY}/.git"
 cd "${OUTPUT_DIRECTORY}"
 touch .nojekyll
@@ -30,6 +31,6 @@ touch .nojekyll
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 git add .
-git commit -m "Push to Github Pages"
+git commit -m "Publish ${DEPLOY_TARGET}"
 
 git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags --force
