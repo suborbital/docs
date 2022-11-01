7b3c3f02aa5f8c4f35a5b87d886cd00c8180708f
Laura Langdon <48335772+LauraLangdon@users.noreply.github.com>
2022-11-01 13:59:58 -0700

    reorganize categories for ease of use

--- website/docs/reference/intro-to-webassembly/history.md
+++ website/docs/reference/intro-to-webassembly/history.md
@@ -7,13 +7,13 @@ pagination_next: null
 The World Wide Web originally started out as a document exchange network to
 meet the demand for automated information-sharing between academics across the world.
 
-![First proposal for World Wide Web, Credit: CERN](../assets/FirstProposalMarch1989.jpg)
+![First proposal for World Wide Web, Credit: CERN](../../assets/FirstProposalMarch1989.jpg)
 
 Running on a NeXT computer at CERN, the [first ever Web page](http://info.cern.ch/hypertext/WWW/TheProject.html) was a simple HTML document containing links to the project itself and a few other technical details. 
 The first ever web browser had many features of current Web browsers and also 
 included the ability to modify pages from directly inside the browser.
 
-![](../assets/web-page-editing.jpg)
+![](../../assets/web-page-editing.jpg)
 
 With its ubiquity and maturation, the complexity of this network and its 
 associated technologies grew. Not only was it required for it to be available
--- website/sidebars.js
+++ website/sidebars.js
@@ -5,28 +5,25 @@ module.exports = {
       label: 'Getting Started with Suborbital',
       collapsible: false,
       items: [
-        'subo',
+        'se2/se2',
+        'se2/quickstart'
       ]
     },
     {
       type: 'category',
-      label: 'Suborbital Extension Engine (SE2)',
+      label: 'How-To',
       collapsible: false,
-      link: {
-        type: 'doc',
-        id: 'se2/se2',
-      },
       items: [
-        'se2/get-started',
-        'se2/using-api',
+        'se2/how-to/integrate-your-app',
+        'se2/how-to/using-api',
         {
-          'Customizing functions': [
-            'se2/customizing-functions/code-editor',
-            'se2/customizing-functions/custom-function-templates',
-            'se2/customizing-functions/custom-libraries',
-            'se2/customizing-functions/namespaces',
-            'se2/customizing-functions/fully-qualified-function-names'
-          ]
+        'Customizing functions': [
+          'se2/how-to/customizing-functions/code-editor',
+          'se2/how-to/customizing-functions/custom-function-templates',
+          'se2/how-to/customizing-functions/custom-libraries',
+          'se2/how-to/customizing-functions/namespaces',
+          'se2/how-to/customizing-functions/fully-qualified-function-names'
+        ]
         },
         {
           type: 'category',
@@ -35,98 +32,19 @@ module.exports = {
           items: [
             {
               'Cloud deployment': [
-                'se2/deployment/cloud-deployment/configure-capabilities',
-                'se2/deployment/cloud-deployment/configure-storage',
-                'se2/deployment/cloud-deployment/configure-webhooks',
-                'se2/deployment/cloud-deployment/install-se2-in-your-cloud-environment'
+                'se2/how-to/deployment/cloud-deployment/configure-capabilities',
+                'se2/how-to/deployment/cloud-deployment/configure-storage',
+                'se2/how-to/deployment/cloud-deployment/configure-webhooks',
+                'se2/how-to/deployment/cloud-deployment/install-se2-in-your-cloud-environment'
               ],
               'Other deployments': [
-                'se2/deployment/other-deployments/minikube'
+                'se2/how-to/deployment/other-deployments/minikube'
               ]
             },
           ]
         },
       ]
     },
-      {
-        type: 'category',
-        label: 'Atmo',
-        collapsible: true,
-        link: {
-          type: 'doc',
-          id: 'atmo/atmo'
-        },
-        items: [
-          {
-            type: 'doc',
-            id: 'atmo/get-started',
-          },
-          {
-            type: "doc",
-            id: 'atmo/glossary'
-          },
-          {
-            collapsed: false,
-            type: 'category',
-            label: 'Concepts',
-            items: [
-              'atmo/concepts/runnables',
-              'atmo/concepts/the-directive',
-              'atmo/concepts/state',
-              'atmo/concepts/static-directory'
-            ]
-          },
-          {
-            type: 'category',
-            label: 'Usage',
-            items: [
-              'atmo/usage/creating-runnables',
-              'atmo/usage/creating-handlers',
-              'atmo/usage/build-your-application',
-              'atmo/usage/deploying-atmo',
-              'atmo/usage/authentication',
-              'atmo/usage/error-handling',
-              'atmo/usage/headless',
-              'atmo/usage/managing-state',
-              'atmo/usage/schedules',
-              'atmo/usage/streams',
-              'atmo/usage/connections',
-            ]
-          },
-          {
-            type: 'category',
-            label: 'Runnable API',
-            link: {
-              type: 'doc',
-              id: 'atmo/runnable-api/introduction'
-            },
-            items: [
-                'atmo/runnable-api/language-support',
-              'atmo/runnable-api/request',
-              'atmo/runnable-api/response',
-              'atmo/runnable-api/logging',
-              'atmo/runnable-api/http-client',
-              'atmo/runnable-api/graphql-client',
-              'atmo/runnable-api/sql-databases',
-              'atmo/runnable-api/cache',
-              'atmo/runnable-api/file',
-            ]
-          },
-        ]
-      },
-      {
-        type: 'category',
-        label: 'Sat',
-        link: {
-          type: 'doc',
-          id: 'sat/sat'
-        },
-        items: [
-          'sat/using-sat',
-          'sat/building-sat',
-          'sat/constellations',
-        ]
-      },
     {
       type: 'category',
       label: 'Reference',
@@ -142,21 +60,107 @@ module.exports = {
           label: 'SE2 Go library reference',
           href: 'https://github.com/suborbital/compute-go',
         },
+        {
+          type: 'doc',
+          id: 'reference/glossary',
+        },
+        {
+          type: 'category',
+          label: 'Intro to WebAssembly',
+          collapsible: true,
+          link: {
+            type: 'doc',
+            id: 'reference/intro-to-webassembly/intro-to-wasm',
+          },
+          items: [
+            'reference/intro-to-webassembly/why-webassembly',
+            'reference/intro-to-webassembly/history'
+          ]
+        },
       ]
     },
     {
       type: 'category',
-      label: 'Intro to WebAssembly',
-      collapsible: true,
-      link: {
-        type: 'doc',
-        id: 'intro-to-webassembly/intro-to-wasm',
-      },
+      label: 'Open Source @ Suborbital',
       items: [
-        'intro-to-webassembly/why-webassembly',
-        'intro-to-webassembly/history'
+        'open-source/subo',
+        {
+          type: 'category',
+          label: 'Atmo',
+          collapsible: true,
+          link: {
+            type: 'doc',
+            id: 'open-source/atmo/atmo'
+          },
+          items: [
+            {
+              type: 'doc',
+              id: 'open-source/atmo/get-started',
+            },
+            {
+              collapsed: false,
+              type: 'category',
+              label: 'Concepts',
+              items: [
+                'open-source/atmo/concepts/runnables',
+                'open-source/atmo/concepts/the-directive',
+                'open-source/atmo/concepts/state',
+                'open-source/atmo/concepts/static-directory'
+              ]
+            },
+            {
+              type: 'category',
+              label: 'Usage',
+              items: [
+                'open-source/atmo/usage/creating-runnables',
+                'open-source/atmo/usage/creating-handlers',
+                'open-source/atmo/usage/build-your-application',
+                'open-source/atmo/usage/deploying-atmo',
+                'open-source/atmo/usage/authentication',
+                'open-source/atmo/usage/error-handling',
+                'open-source/atmo/usage/headless',
+                'open-source/atmo/usage/managing-state',
+                'open-source/atmo/usage/schedules',
+                'open-source/atmo/usage/streams',
+                'open-source/atmo/usage/connections',
+              ]
+            },
+            {
+              type: 'category',
+              label: 'Runnable API',
+              link: {
+                type: 'doc',
+                id: 'open-source/atmo/runnable-api/introduction'
+              },
+              items: [
+                'open-source/atmo/runnable-api/language-support',
+                'open-source/atmo/runnable-api/request',
+                'open-source/atmo/runnable-api/response',
+                'open-source/atmo/runnable-api/logging',
+                'open-source/atmo/runnable-api/http-client',
+                'open-source/atmo/runnable-api/graphql-client',
+                'open-source/atmo/runnable-api/sql-databases',
+                'open-source/atmo/runnable-api/cache',
+                'open-source/atmo/runnable-api/file',
+              ]
+            },
+          ]
+        },
+        {
+          type: 'category',
+          label: 'Sat',
+          link: {
+            type: 'doc',
+            id: 'open-source/sat/sat'
+          },
+          items: [
+            'open-source/sat/using-sat',
+            'open-source/sat/building-sat',
+            'open-source/sat/constellations',
+          ]
+        },
       ]
-    },
+    }, 
     {
       type: 'category',
       label: 'Contributing to Suborbital',
